19aa958918f814c75092e290ba908f7d70107d9d
Greg Greggaries <mihkeland@proton.me>
2026-03-23 16:48:08 -0700

    cleaned up 'show version' script

--- show_version.py
+++ show_version.py
@@ -10,21 +10,38 @@ on a Cisco Nexus 9300v image running NX-OS version 10.5(1) on the CML-2 platform
 
 """
 
-myheaders={'content-type':'application/json'}
-payload={
-  "ins_api": {
-    "version": "1.0",
-    "type": "cli_show",
-    "chunk": "0",
-    "sid": "sid",
-    "input": "show version",
-    "output_format": "json"
+def get_version():
+  myheaders={'content-type':'application/json'}
+  payload={
+    "ins_api": {
+      "version": "1.0",
+      "type": "cli_show",
+      "chunk": "0",
+      "sid": "sid",
+      "input": "show version",
+      "output_format": "json"
+    }
   }
-}
 
-response = requests.post(host,data=json.dumps(payload), headers=myheaders,auth=(username,pword), verify=False).json()["ins_api"]["outputs"]["output"]["body"]
+  response = requests.post(host,data=json.dumps(payload), headers=myheaders,auth=(username,pword), 
+                         verify=False).json()["ins_api"]["outputs"]["output"]["body"]
+  return response
 
-print(f"Command output:\n{json.dumps(response, indent=2)}")
+def main():
+  print(f"This is us running the 'get version'command on a Cisco Nexus 9300v")
+  command_output = get_version()
+  version = command_output["nxos_ver_str"]
+  file_name = command_output["nxos_file_name"]
+  chassis_id = command_output["chassis_id"]
+  cpu_name = command_output["cpu_name"]
+  memory = command_output["memory"]
+  
+  print()
+  print(f"Software version: {version}")
+  print(f"File name: {file_name}")
+  print(f"Chassis ID: {chassis_id}")
+  print(f"CPU type: {cpu_name}")
+  print(f"Memory: {memory}")
 
-
-
+if __name__ == "__main__":
+  main()
